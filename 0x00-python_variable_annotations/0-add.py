654918539cac527f45d427ffec05916b9466dd4d
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-11 11:10:53 +0200

    fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+'''
+file contains function to calc the sum of 2 numbers
+'''
 def add(a: float, b: float) -> float:
+    '''
+    function to return sum of two numbers as a float 
+    '''
     return a+b
